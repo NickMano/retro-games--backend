04825eacfc8b93bf8890c00199f4a32dc5ff4f97
Nico <nmanograsso@gmail.com>
2020-12-25 16:22:51 -0300

    add mock of games

---
+++ utils/mocks/gamesMock.js
@@ -0,0 +1,16 @@
+const mock = [{
+    "id": 2,
+    "title": "Brawlhalla",
+    "genre": ["Action"],
+    "release": 2019,
+    "contentRating": "16+",
+    "platform": ["Nintendo Switch", "PC", "PS5"],
+    "cover": "https://static.displate.com/857x1200/displate/2020-06-17/7014f73b75d6122881499c12ebdf2603_53513349e4da448a8d89a0da4847f239.jpg",
+    "description": "Vestibulum ac est lacinia nisi venenatis tristique",
+    "videos": ["https://mdstrm.com/video/58333e214ad055d208427db5.mp4"],
+    "images": [""],
+    "company": "Square Enix",
+    "players": "1-8"
+  }]
+
+  module.exports = mock
