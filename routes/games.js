209a730afa299d651f1b8a5e9db54c7d4542f9cc
Nico <nmanograsso@gmail.com>
2020-12-25 17:27:32 -0300

    add crud to games

--- routes/games.js
+++ routes/games.js
@@ -16,6 +16,60 @@ const gamesApi = (app) => {
             next(err)
         }
     })
+
+    router.get('/:gameId', async function(req, res, next) {
+        try {
+          const games = await Promise.resolve(gamesMock[0]);
+    
+          res.status(200).json({
+            data: games,
+            message: 'game retrieved'
+          });
+        } catch (err) {
+          next(err);
+        }
+      });
+    
+    
+      router.post('/', async function(req, res, next) {
+        try {
+          const createdGameId = await Promise.resolve(gamesMock[0].id);
+    
+          res.status(201).json({
+            data: createdGameId,
+            message: 'game created'
+          });
+        } catch (err) {
+          next(err);
+        }
+      });
+    
+    
+      router.put('/:gameId', async function(req, res, next) {
+        try {
+          const updatedGameId = await Promise.resolve(gamesMock[0].id);
+    
+          res.status(200).json({
+            data: updatedGameId,
+            message: 'game updated'
+          });
+        } catch (err) {
+          next(err);
+        }
+      });
+    
+      router.delete('/:gameId', async function(req, res, next) {
+        try {
+          const deletedGameId = await Promise.resolve(gamesMock[0].id);
+    
+          res.status(200).json({
+            data: deletedGameId,
+            message: 'game deleted'
+          });
+        } catch (err) {
+          next(err);
+        }
+      });
 }
 
 module.exports = gamesApi
