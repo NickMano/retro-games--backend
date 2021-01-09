df3893817c9d25ee0829ba0a93354c7648b1ce9e
Nico <nmanograsso@gmail.com>
2021-01-09 11:51:22 -0300

    add middleware to handle errors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { config } = require('./config/index');
 const gamesApi = require('./routes/games');
+const {
+  logErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 gamesApi(app);
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
